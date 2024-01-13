fe5b7e5c25ec06f4b051a4c49ecf8786cc4a2779
DevX <63361968+010DevX101@users.noreply.github.com>
2024-01-13 12:59:17 -0300

    Update Prompt.lua

--- Examples/Prompt.lua
+++ Examples/Prompt.lua
@@ -5,5 +5,5 @@ local ColorPicker = ColorPicker:Prompt(
 	plugin, -- Plugin Instance
 	"ColorPicker", -- Gui Id
 	"Color Picker", -- Gui Title
-    Color3.fromRGB(255,70,100) -- Initial Color
+	Color3.fromRGB(255,70,100) -- Initial Color
 )
